f7fa243b08faced4fb72e69f1f55307b75e27a51
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-09-07 16:38:31 +0100

    Add comment to existing functions and constants

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,6 @@
+/*  This element is the main container where the squares will be displayed. */
 const container = document.querySelector(".container");
-
+/* This constant is used as a limit for the number of squares that can be generated per row in the grid. */
 const MAX_LIMIT = 100;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,6 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
   generateButton();
 });
 
+/**
+ * The function `alterMainContainer` sets the display, box sizing, padding, and margin properties of
+ * the container element to specific values.
+ */
 function alterMainContainer() {
   container.style.display = "flex";
   container.style.boxSizing = "border-box";
@@ -14,6 +19,10 @@ function alterMainContainer() {
   container.style.margin = "0";
 }
 
+/**
+ * The function `generateButton` creates a button element with specific styling and event listener to
+ * prompt the user for a number.
+ */
 function generateButton() {
   const btn = document.createElement("button");
   btn.textContent = "Set Square number";
@@ -23,6 +32,13 @@ function generateButton() {
   container.appendChild(btn);
 }
 
+/**
+ * The function `promptNumber` prompts the user to enter a number of squares per row within a specified
+ * range and generates a grid based on the input.
+ * @returns The `promptNumber` function returns `null` if the prompt is cancelled by the user,or an alert message
+ * in case the user placed an invalid input on the prompt text area.
+ * Otherwise, it generates a grid based on the number entered by the user.
+ */
 function promptNumber() {
   let number;
   do {
