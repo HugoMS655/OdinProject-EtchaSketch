7566554f47a7d410f387fc57f6758b2795e86cef
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-09-07 15:06:59 +0100

    Add basic logic to js file , still to test
    added function to generate button, the grid,
    the max limit of squares, and the main container
    element to be used as a reference to add other
    html elements later namely the div with squares

---
+++ etchasketch.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const MAX_LIMIT = 100;
+
+function generateButton() {
+  const btn = document.createElement("button");
+  btn.textContent = "Set Square number";
+  btn.style.fontSize = 20;
+  container.appendChild(btn);
+}
+
+function promptDivNumber() {}
+
+function generateGrid(numberOfSquares) {}
